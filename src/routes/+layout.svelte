3a2bbddbe73cdc1e688790a977e40df23aecc854
jul-cai-dev <jul.cai.dev@gmail.com>
2022-11-10 08:49:29 +0100

    feat: added script ref to injected script for vercel audience

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
-<script>
-    import "../app.css";
+<script lang="ts">
+  import "../app.css";
 </script>
 
 <svelte:head>
 	<title>Julian CAILLE - Software Engineer</title>
   <meta name="description" content="Julian CAILLE's resume" />
+  <script defer src="/va/script.js"></script>
 </svelte:head>
 
 <slot />
